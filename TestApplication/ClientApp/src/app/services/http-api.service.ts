b2fcc089849da3e27df2239a8b62fc5171732fda
Oleg Tarnarutsky <olegtar83@gmail.com>
2020-05-23 12:51:32 +0300

    Adding exception handling

--- TestApplication/ClientApp/src/app/services/http-api.service.ts
+++ TestApplication/ClientApp/src/app/services/http-api.service.ts
@@ -1,9 +1,10 @@
 import { Injectable } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
-import { Observable } from 'rxjs';
+import { Observable, of } from 'rxjs';
 import { IVisitedDepartments } from '../models/visitedDepartments';
 import { IDepartment } from '../models/department';
 import { IActiveEmployees } from '../models/activeEmployees';
+import { tap, catchError } from 'rxjs/operators';
 
 
 @Injectable({
@@ -15,12 +16,31 @@ export class HttpApiService {
   private baseUrl: string = '';
 
   public getAllDepartments(): Observable<IDepartment[]>{
-    return this.http.get<any>(`${this.baseUrl}api/Common/getDepartments`); 
+    return this.http.get<any>(`${this.baseUrl}api/Common/getDepartments`).pipe(
+      catchError(this.handleError<any>('get'))
+    ); 
   }
   public getMostActiveEmployees(): Observable<IActiveEmployees[]>{
-    return this.http.get<any>(`${this.baseUrl}api/Common/getMostActiveEmployees`); 
+    return this.http.get<any>(`${this.baseUrl}api/Common/getMostActiveEmployees`).pipe(
+      catchError(this.handleError<any>('get'))
+    ); 
   }
   public getMostVisitedDepartments(): Observable<IVisitedDepartments[]>{
-    return this.http.get<any>(`${this.baseUrl}api/Common/getMostVisitedDepartments `); 
+    return this.http.get<any>(`${this.baseUrl}api/Common/getMostVisitedDepartments `).pipe(
+      catchError(this.handleError<any>('get'))
+    );
+  }
+  private handleError<T>(operation = 'operation', result?: T) {
+    return (error: any): Observable<T> => {
+
+      // TODO: send the error to remote logging infrastructure
+      console.error(error.error.title); // log to console instead
+
+      // TODO: better job of transforming error for user consumption
+      console.log(`${operation} failed: ${error.title}`);
+
+      // Let the app keep running by returning an empty result.
+      return of(result as T);
+    };
   }
 }
